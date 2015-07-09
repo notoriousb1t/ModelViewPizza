d5ef7b9a7ddba0ccb1161479549b4943d0f74a5b
Christopher Wallis <cwallis@ChristophersMBP.home>
2015-07-09 00:32:08 -0400

    removed isCustom from NG example.  added summary comments to js file

--- projects/angularjs1/ng-app.js
+++ projects/angularjs1/ng-app.js
@@ -1,11 +1,13 @@
 /// <reference path="../../typings/angular/angular1.d.ts" />
 (function () {
     'use strict';
+    // import shared settings from config on window
     var config = window.config;
     var initial = config.initial;
     var presets = config.presets;
     var options = config.options;
 
+    // define the angular module 'app'
     var app = angular.module('app', []);
 
     app.controller('OrderController', ['$scope', function ($scope) {
@@ -24,31 +26,35 @@
         $scope.toppings = [];
 		
         // computed values
-        $scope.isCustom = function () {
-            return $scope.preset === initial.preset;
-        };
         $scope.hasTopping = function (topping) {
+            ///<summary>returns true if topping is selected</summary>
             return $scope.toppings.indexOf(topping) !== -1;
         };
         $scope.nextText = function () {
+            ///<summary>returns text to display in the next button</summary>
             var isLastStep = $scope.stepNames.length - 1 == $scope.step;
-            return isLastStep ? 'Restart' : 'Next';
+            if (isLastStep) {
+                return 'Restart';
+            }
+            return 'Next';
         };
 		
         // events
         $scope.toggleTopping = function (topping) {
-            // get index of topping
+            ///<summary>adds/removes toppings from the selected toppings</summary>
+            // find topping index
             var indexOfTopping = $scope.toppings.indexOf(topping);
             if (indexOfTopping === -1) {
-                // add topping if not exist
+                // add topping if it does not exist
                 $scope.toppings.push(topping);
                 return;
             }
 			
-            // remove topping
+            // remove topping if it exists
             $scope.toppings.splice(indexOfTopping, 1);
         };
         $scope.prev = function () {
+            ///<summary>moves to the previous step</summary>
             var newStep = $scope.step - 1;
             if (newStep < 0) {
                 newStep = 0;
@@ -56,14 +62,16 @@
             $scope.step = newStep;
         };
         $scope.next = function () {
+            ///<summary>moves to the next step or reloads if on last step</summary>            
             var newStep = $scope.step + 1
             if (newStep >= $scope.stepNames.length) {
                 window.location.reload();
             }
             $scope.step = newStep;
         };
-        // watch the preset
+        
         $scope.$watch('preset', function (newValue, oldValue) {
+            ///<summary>changes the pizza settings when a preset is selected</summary>            
             if (newValue === oldValue) {
                 // skip updates when the preset hasn't changed
                 return;
@@ -73,17 +81,16 @@
             $scope.toppings = [];
 
             for (var presetName in presets) {
-                // skip presets that don't match this
                 if (presetName !== newValue) {
+                    // skip presets that don't match this
                     continue;
                 }
 
+                // get preset from presets
                 var preset = presets[presetName];
 				
-                // change sauce to preset
+                // change sauce and toppings to preset
                 $scope.sauce = preset.sauce;
-				
-                // add all preset toppings into
                 $scope.toppings = preset.toppings.slice(0);
                 break;
             }
